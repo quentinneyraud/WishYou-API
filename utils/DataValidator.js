51eb5d8229d9bd2612d12d6c1d4af372b56132a2
Quentin Neyraud <quentin.neyraud@gmail.com>
2016-01-13 01:12:04 +0100

    add data validator

---
+++ utils/DataValidator.js
@@ -0,0 +1,41 @@
+var validator = require('validator');
+
+module.exports = {
+
+    init: function(){
+        this.errors = [];
+        return this;
+    },
+
+    validateString: function(string, minChars, maxChars){
+        if(!validator.isAlpha(string)){
+            this.errors.push('"' + string + '" is not a valid string')
+        }else if(string.length < minChars || string.length > maxChars){
+            this.errors.push("'" + string + "' is not valid. It must be between " + minChars + " and " + maxChars + " characters")
+        }
+        return this;
+    },
+
+    validateEmail: function(email){
+        if(!validator.isEmail(email)){
+            this.errors.push("'" + email + "' is not a valid email.")
+        }
+        return this;
+    },
+
+    validateLength: function(string, minChars, maxChars){
+        if(string.length < minChars || string.length > maxChars){
+            this.errors.push("'" + string + "' is not valid. It must be between " + minChars + " and " + maxChars + " characters")
+        }
+        return this;
+    },
+
+    getErrors: function(){
+        return this.errors;
+    },
+
+    issetErrors: function(){
+        return this.errors.length > 0;
+    }
+
+}
